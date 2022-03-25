517e530b3299f99a10481f41668f27f13ac57637
Vyvy-vi <vyom.j@protonmail.com>
2022-03-25 20:58:07 +0530

    messageCreate

--- index.js
+++ index.js
@@ -3,24 +3,17 @@ const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
 // discord bot connection instance
-const client = new Client({ intents: Intents.FLAGS.GUILDS });
-
-/*
-function t() {
-	console.log("Ready!");
-}
-
-client.on("ready", t);
-
-function sum(a, b) {
-	return a + b;
-}
-
-const sum = (a, b) => a + b;
-*/
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on("ready", () => {
 	console.log("Hello world! (in terminal)");
+});
+
+client.on("messageCreate", (message) => {
+	console.log("test");
+	console.log(message);
 });
 
 // Login to Discord with your client's token
